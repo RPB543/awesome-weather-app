fb436f134529934df66301a418cc0396160a3ae9
Becca Byrne <aggiebec06@gmail.com>
2021-11-08 17:47:47 -0600

    updated formatting in BS and CSS

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 var input = document.querySelector("#search");
 var form = document.querySelector(".form");
-var icon = document.querySelector(".weather-icon");
-var temp = document.querySelector(".temperature value p");
-var summary = document.querySelector(".temperature-description p");
 var submitEl = document.querySelector("#search-btn");
-const kelvin = 273;
+
 
 const key = "bae57e4cda117c8b12b4f90bdd90b054";
-const base = "https://api.openweathermap.org/data/2.5/";
 
 // get name of city from input
 submitEl.addEventListener("click", function(){
@@ -21,14 +17,18 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + 
   .then(function(response) {
     return response.json();
   })
-  .then(function(myJson) {
-    console.log(myJson);
+  .then(function(data) {
+    console.log(data);
     var weatherCard = $("<div>").addClass("card");
-    var cardTitle = $("<h3>").addClass("card-title").text(myJson.name);
-    var tempEl = $("<p>").addClass("card-text").text(myJson.main.temp);
-    var imgEl = $("<img>").attr("src", "https://openweathermap.org/img/w/" + myJson.weather[0].icon + ".png");
-    $(".weather-container").append(weatherCard.append(cardTitle.append(imgEl), tempEl));
+    var cardTitle = $("<h3>").addClass("card-title").text(data.name);
+    // var dateEl = $("<h3>").addClass("card-title").text(data.dt).toLocaleDateString("en-US");
+    var tempEl = $("<p>").addClass("card-text").text("Temp: " + data.main.temp + "°F");
+    var imgEl = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+    var windEl = $("<p>").addClass("card-text").text("Wind: " + data.wind.speed + " mph");
+    var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+    $(".weather-container").append(weatherCard.append(cardTitle.append(imgEl), tempEl, windEl, humidityEl));
   });
 }
+function getCoordinates() {
 
-// console.log(results) from API
+}
