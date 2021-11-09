1ad17cbc59f920d0250cbe86d68ab13bacc30902
Becca Byrne <aggiebec06@gmail.com>
2021-11-09 17:52:17 -0600

    added UV index

--- script.js
+++ script.js
@@ -18,35 +18,42 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + 
   })
   .then(function(data) {
     console.log(data);
-    var weatherCard = $("<div>").addClass("card");
-    var cardTitle = $("<h3>").addClass("card-title").text(data.name + " (" + new Date(data.dt*1000).toLocaleDateString("en-US") + ")");
-    var tempEl = $("<p>").addClass("card-text").text("Temp: " + data.main.temp + "°F");
-    var imgEl = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-    var windEl = $("<p>").addClass("card-text").text("Wind: " + data.wind.speed + " mph");
-    var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
     var coordsEl = 'lat=' + data.coord.lat + '&lon=' + data.coord.lon;
     getForecast(coordsEl);
-    $(".weather-container").append(weatherCard.append(cardTitle.append(imgEl), tempEl, windEl, humidityEl));
+    getUvIndex(coordsEl);
   });
+  input.textContent = input
 }
 
-// research open weather API for 5 day forecast
+function getUvIndex(coordsEl) {
+  fetch('https://api.openweathermap.org/data/2.5/onecall?' + coordsEl + '&exclude=hourly,minutely&appid=' + key + '&units=imperial')
+  .then(function(response) {
+    response.json().then(function(data){
+    var weatherCard = $("<div>").addClass("card");
+    var cardTitle = $("<h3>").addClass("card-title").text(input.value + " (" + new Date(data.current.dt*1000).toLocaleDateString("en-US") + ")");
+    var tempEl = $("<p>").addClass("card-text").text("Temp: " + data.current.temp + "°F");
+    var imgEl = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
+    var windEl = $("<p>").addClass("card-text").text("Wind: " + data.current.wind_speed + " mph");
+    var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + data.current.humidity + " %");
+   var uvIndex = $("<p>").addClass("card-text").text("UV Index: " + data.current.uvi);
+   $(".weather-container").append(weatherCard.append(cardTitle.append(imgEl), tempEl, windEl, humidityEl, uvIndex))
+});
+  })
+ return getUvIndex;
+  }
+
+// create function to pull 5 day forecast
 function getForecast(coordsEl) {
-  let otherDayForecast = "";
+
+// create loop to get 5-day forecast
   for(let i=1; i <= 5; i++){
   fetch('https://api.openweathermap.org/data/2.5/onecall?' + coordsEl + '&exclude=hourly,minutely&appid=' + key + '&units=imperial')
   .then(function(response) {
-    response.json().then(function(forecast){
-
-    // create a loop to obtain forecast each day
-    // forecast.daily.forEach(function(idx) {
-    //  if(idx == 0) {
-
-    //  }else {
-    //  otherDayForecast += ""
    
+    response.json().then(function(forecast){
+      console.log(forecast)
     // create card to hold each day's data
-    var dailyForecast = $("<div>").addClass("card");
+    var dailyForecast = $("<div>").addClass("card col-2 w-5");
 
     // create elements for date, temp, icon, wind and humidity
     var cardTitle = $("<h4>").addClass("card-title").text(new Date(forecast.daily[i].dt*1000).toLocaleDateString("en-US"));
@@ -55,8 +62,8 @@ function getForecast(coordsEl) {
     var windEl = $("<p>").addClass("card-text").text("Wind: " + forecast.daily[i].wind_speed + " mph");
     var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + forecast.daily[i].humidity + " %");
   
-    //apend the elements to the forecast conatiner
-    $(".weather-forecast").append(dailyForecast.append(cardTitle, imgEl, tempEl, windEl, humidityEl));
+    //append the elements to the forecast container
+    $(".weather-forecast").append(dailyForecast.append(cardTitle.append(imgEl), tempEl, windEl, humidityEl));
          });
     });
   };
